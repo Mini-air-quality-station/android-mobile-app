8de53b4876168f57e70db9e543f5e4b2e240d12a
Wojciech Panfil <wojtek.panfil@hotmail.com>
2023-01-24 10:45:23 +0100

    android-mobile-app: Add menu with not working buttons
    Buttons are not yet required, but a template has been left in the code.
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

--- app/src/main/java/station/quality/air/mini/app/MainActivity.java
+++ app/src/main/java/station/quality/air/mini/app/MainActivity.java
@@ -1,23 +1,66 @@
 package station.quality.air.mini.app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private WebView webView_config;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private String curr_url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.openNavDrawer, R.string.closeNavDrawer
+        );
+
+
+        actionBarDrawerToggle.syncState();
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()){
+                case R.id.url_menu_1: setUrl("http://127.0.0.1"); reloadWebsite(); break;
+                case R.id.url_menu_2: setUrl("https://trojmiasto.pl"); reloadWebsite(); break;
+            }
+            return true;
+        });
+
+         //Change this one day - it's special macro for dev machines localhost
         webView_config = findViewById(R.id.webView_config);
         webView_config.getSettings().setJavaScriptEnabled(true);
-        webView_config.loadUrl("http://10.0.2.2"); //Change this one day - it's special macro for dev machines localhost
+        setUrl("http://10.0.2.2");
+        reloadWebsite();
+    }
+
+    void setUrl(String url){
+        this.curr_url = url;
+    }
+
+    void reloadWebsite(){
+        webView_config.loadUrl(this.curr_url);
     }
 
     @Override
@@ -27,5 +70,26 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        System.out.println("Hello there@!");
+        if (item.getItemId() == R.id.url_menu_1) {
+            setUrl("https://trojmiasto.pl");
+            reloadWebsite();
+            return true;
+        }
+        else if (item.getItemId() == R.id.url_menu_2){
+            setUrl("http://127.0.0.1");
+            reloadWebsite();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
